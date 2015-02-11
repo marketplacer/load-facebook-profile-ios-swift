2ef3d5eb15c49847270f3a927f60fd0655c94b5f
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-02-11 13:50:19 +1100

    Login

--- LoginWithFacebookSDKCustom/ViewController.swift
+++ LoginWithFacebookSDKCustom/ViewController.swift
@@ -9,32 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var loginLogoutButton: UIButton!
+  @IBOutlet weak var userInfoLabel: UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    userInfoLabel.text = ""
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
 
   @IBAction func onLoginWithFacebookButtonTapped(sender: AnyObject) {
+    userInfoLabel.text = ""
+    
+    FBSession.openActiveSessionWithReadPermissions(["public_profile"],
+      allowLoginUI: true) { session, state, error in
 
-    if (FBSession.activeSession().state == FBSessionState.Open
-      || FBSession.activeSession().state == FBSessionState.OpenTokenExtended) {
-
-      // Logout
-      FBSession.activeSession().closeAndClearTokenInformation()
-      println("closeAndClearTokenInformation")
-    } else {
-      FBSession.openActiveSessionWithReadPermissions(["public_profile"],
-        allowLoginUI: true) { session, state, error in
-
-        println("openActiveSessionWithReadPermissions callback")
-      }
+      self.sessionStateChanged(session, state: state, error: error)
     }
+  }
+  
+  private func showUserInfo(session: FBSession) {
+    if let currentAccessToken = session.accessTokenData {
+      let acessToken = currentAccessToken.accessToken
+      
+      FBRequestConnection.startForMeWithCompletionHandler { connection, result, error in
+        if error != nil { return }
+        if let currentResult = result as? NSDictionary {
+          if let userId = result["id"] as? String {
+            self.userInfoLabel.text = "\(rand()) User ID: \(userId) \n\nAccess token: \(acessToken)"
+          }
+        }
+      }
+      
+    }
+    
+    // Note: to verify user id send this request from your server
+    // https://graph.facebook.com/me?fields=id&access_token=YOUR_ACCCESS_TOKEN
+  }
+  
+  private func sessionStateChanged(session: FBSession, state: FBSessionState, error: NSError?) {
+    if error != nil { return }
+    
+    showUserInfo(session)
   }
 }
 
