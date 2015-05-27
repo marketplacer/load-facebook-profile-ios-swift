c8f00c1fac102bb64ced6b012977f855969df80b
Anthony Mittaz <anthony@marketplacer.com>
2015-05-27 15:16:54 +1000

    update Facebook SDK (4.1.0), introduce factory (useful for testing)

--- LoadFacebookProfile/TegFacebookUserLoader.swift
+++ LoadFacebookProfile/TegFacebookUserLoader.swift
@@ -1,69 +1,109 @@
 import Foundation
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class TegFacebookUserLoader {
+protocol FacebookUserLoader: class {
+  func load(#askEmail: Bool, onSuccess: (TegFacebookUser)->())
+  func cancel()
+}
+
+class FacebookUserLoaderFactory {
+  class var userLoader: FacebookUserLoader {
+    if isKIFing() {
+      return FakeFacebookUserLoader()
+    } else {
+      return TegFacebookUserLoader()
+    }
+  }
   
-  class func load(
-    #askEmail: Bool,
-    onSuccess: (TegFacebookUser)->()) {
-      
+  private class func isKIFing() -> Bool {
+    return NSClassFromString("KIFTestActor") != nil
+  }
+}
+
+class TegFacebookUserLoader: FacebookUserLoader {
+  private let loginManager: FBSDKLoginManager
+  private var currentConnection: FBSDKGraphRequestConnection?
+  
+  init() {
+    loginManager = FBSDKLoginManager()
+  }
+  
+  deinit {
+    cancel()
+  }
+  
+  func load(#askEmail: Bool, onSuccess: (TegFacebookUser)->()) {
+    cancel()
     logOut()
     logInAndLoadUserProfile(askEmail, onSuccess: onSuccess)
   }
   
-  private class func logInAndLoadUserProfile(
-    askEmail: Bool,
-    onSuccess: (TegFacebookUser)->()) {
-      
+  func cancel() {
+    currentConnection?.cancel()
+    currentConnection = nil
+  }
+  
+  private func logInAndLoadUserProfile(askEmail: Bool, onSuccess: (TegFacebookUser)->()) {
     var permissions = ["public_profile"]
-      
+    
     if askEmail {
       permissions.append("email")
     }
-      
-    FBSession.openActiveSessionWithReadPermissions(permissions,
-      allowLoginUI: true) { session, state, error in
-      
+    
+    loginManager.logInWithReadPermissions(permissions) { [weak self] result, error in
       if error != nil { return }
-      if state == FBSessionState.Closed { return }
+      if result.isCancelled { return }
       
-      self.loadFacebookMeInfo(onSuccess)
+      self?.loadFacebookMeInfo(onSuccess)
     }
   }
   
-  private class func logOut() {
-    FBSession.activeSession()?.closeAndClearTokenInformation()
+  private func logOut() {
+    loginManager.logOut()
   }
   
-  private class func loadFacebookMeInfo(onSuccess: (TegFacebookUser) -> ()) {
-      
-    FBRequestConnection.startForMeWithCompletionHandler { connection, result, error in
+  private func loadFacebookMeInfo(onSuccess: (TegFacebookUser) -> ()) {
+    if FBSDKAccessToken.currentAccessToken() == nil { return }
+    currentConnection = FBSDKGraphRequest(graphPath: "me", parameters: nil).startWithCompletionHandler { [weak self] connection, result, error in
       if error != nil { return }
       
       if let userData = result as? NSDictionary,
-        let user = self.parseMeData(userData) {
-        
-        onSuccess(user)
+        let user = self?.parseMeData(userData) {
+          onSuccess(user)
       }
     }
   }
   
-  private class func parseMeData(data: NSDictionary) -> TegFacebookUser? {
+  private func parseMeData(data: NSDictionary) -> TegFacebookUser? {
     if let id = data["id"] as? String,
       let accessToken = accessToken {
-
-      return TegFacebookUser(
-        id: id,
-        accessToken: accessToken,
-        email: data["email"] as? String,
-        firstName: data["first_name"] as? String,
-        lastName: data["last_name"] as? String,
-        name: data["name"] as? String)
+        return TegFacebookUser(
+          id: id,
+          accessToken: accessToken,
+          email: data["email"] as? String,
+          firstName: data["first_name"] as? String,
+          lastName: data["last_name"] as? String,
+          name: data["name"] as? String)
     }
     
     return nil
   }
   
-  private class var accessToken: String? {
-    return FBSession.activeSession()?.accessTokenData?.accessToken
+  private var accessToken: String? {
+    return FBSDKAccessToken.currentAccessToken().tokenString
+  }
+}
+
+class FakeFacebookUserLoader: FacebookUserLoader {
+  func load(#askEmail: Bool, onSuccess: (TegFacebookUser)->()) {
+    TegQ.runAfterDelay(1) { [weak self] in
+      let fakeUser = TegFacebookUser(id: "fake-user-id", accessToken: "fake-access-token", email: nil, firstName: nil, lastName: nil, name: nil)
+      onSuccess(fakeUser)
+    }
+  }
+  
+  func cancel() {
+    // Do nothing
   }
 }
