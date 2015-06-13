0b16a3db4ac6ab6cf735746bef06452e105f7bc7
Anthony Mittaz <anthony@marketplacer.com>
2015-06-13 15:56:03 +1000

    make TegFacebookUser initializer public

--- LoadFacebookProfileKit/TegFacebookUser.swift
+++ LoadFacebookProfileKit/TegFacebookUser.swift
@@ -10,7 +10,7 @@ public struct TegFacebookUser {
   public let lastName: String?
   public let name: String?
   
-  init(id: String, accessToken: String, email: String?, firstName: String?, lastName: String?, name: String?) {
+  public init(id: String, accessToken: String, email: String?, firstName: String?, lastName: String?, name: String?) {
     self.id = id
     self.accessToken = accessToken
     self.email = email
